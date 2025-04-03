e670aeee3341add7de75433508c6ac0f932b3876
saadhn4 <saadcfi4@gmail.com>
2025-04-03 10:34:34 +0400

    done for now

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,17 @@ import pfp from "../src/assets/cat.jpg";
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [newTweet, setNewTweet] = useState("");
+
+  const postTweet = () => {
+    const trimmed = newTweet.trim();
+    if (!trimmed) {
+      alert("Tweet something...");
+      return;
+    }
+    const newPosts = [...posts];
+    setPosts([...newPosts, trimmed]);
+    setNewTweet("");
+  };
   return (
     <>
       <div className="post-tweet-container">
@@ -16,9 +27,27 @@ const App = () => {
           onChange={(e) => setNewTweet(e.target.value)}
         />
         <div>
-          <button className="btn-primary">Tweet</button>
+          <button onClick={postTweet} className="btn-primary">
+            Tweet
+          </button>
         </div>
       </div>
+      {posts.map((post, index) => (
+        <div key={index} className="tweet-card">
+          <div className="pfp-column">
+            <img src={pfp} alt="profile-pic" className="profile-pic" />
+          </div>
+          <div className="second-column">
+            <div className="top-row">
+              <p className="name">Saad</p>
+              <p className="username">@saad.hn</p>
+            </div>
+            <div className="bottom-row">
+              <p>{post}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
